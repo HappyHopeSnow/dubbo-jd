a694e4a0b146f44f44adfcc772185662b9492060
lianle1 <lianle1@jd.com>
2016-02-15 18:24:16 +0800

    add dubbo-config module. -- by lianle 2016-02-15

---
+++ dubbo-config/dubbo-config-spring/src/main/java/com/alibaba/dubbo/config/spring/AnnotationBean.java
@@ -0,0 +1,10 @@
+package com.alibaba.dubbo.config.spring;
+
+/**
+ * Description: <br>
+ *
+ * @author <a href=mailto:lianle1@jd.com>连乐</a>
+ * @date 2016/2/15 18:23
+ */
+public class AnnotationBean {
+}
